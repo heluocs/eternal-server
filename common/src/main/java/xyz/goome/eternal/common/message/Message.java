aca214532e0d28c55b64f9780efe0f634c6c958a
李星 <568507228@qq.com>
2016-04-24 18:58:38 +0800

    update message

--- common/src/main/java/xyz/goome/eternal/common/message/Message.java
+++ common/src/main/java/xyz/goome/eternal/common/message/Message.java
@@ -11,15 +11,11 @@ public class Message {
     public static final int MSG_ACCOUNT_REGIST_REQUEST_C2S              = 0x00010003;
     public static final int MSG_ACCOUNT_REGIST_RESPONSE_S2C             = 0x00010004;
 
-    public static final int MSG_SERVER_MODULE                           = 0x00020000;
-    public static final int MSG_SERVER_LIST_REQUEST_C2S                 = 0x00020001;
-    public static final int MSG_SERVER_LIST_RESPONSE_S2C                = 0x00020002;
-
-    public static final int MSG_ROLE_MODULE                             = 0x00030000;
-    public static final int MSG_ROLE_LIST_REQUEST_C2S                   = 0x00030001;
-    public static final int MSG_ROLE_LIST_RESPONSE_S2C                  = 0x00030002;
-    public static final int MSG_ROLE_CREATE_REQUEST_C2S                 = 0x00030003;
-    public static final int MSG_ROLE_CREATE_RESPONSE_S2C                = 0x00030004;
-    public static final int MSG_ROLE_DELETE_REQUEST_C2S                 = 0x00030005;
-    public static final int MSG_ROLE_DELETE_RESPONSE_S2C                = 0x00030006;
+    public static final int MSG_ROLE_MODULE                             = 0x00020000;
+    public static final int MSG_ROLE_LIST_REQUEST_C2S                   = 0x00020001;
+    public static final int MSG_ROLE_LIST_RESPONSE_S2C                  = 0x00020002;
+    public static final int MSG_ROLE_CREATE_REQUEST_C2S                 = 0x00020003;
+    public static final int MSG_ROLE_CREATE_RESPONSE_S2C                = 0x00020004;
+    public static final int MSG_ROLE_DELETE_REQUEST_C2S                 = 0x00020005;
+    public static final int MSG_ROLE_DELETE_RESPONSE_S2C                = 0x00020006;
 }
